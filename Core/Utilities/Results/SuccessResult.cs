e332a15701b0575752b267603c7584cd88b5c0b1
Zeynel OK <zeynel_2406@hormail.com>
2021-02-26 22:26:28 +0300

    Add project files.

---
+++ Core/Utilities/Results/SuccessResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class SuccessResult:Result
+    {
+        //Bunu kullanarak yazılımcı dönüşte true döndürüyor ve mesaj döndürüyor
+        public SuccessResult(string message):base(true,message)
+        {
+
+        }
+
+        //Mesaj döndürmek istemezse sadece bool değer döndürmek için bunu kullanacak
+        public SuccessResult():base(true)
+        {
+
+        }
+    }
+}
