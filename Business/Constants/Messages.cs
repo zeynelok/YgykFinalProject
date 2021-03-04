a81c8db3137c847ce653079af4ea0efcf7529628
Zeynel OK <zeynel_2406@hormail.com>
2021-03-04 21:39:16 +0300

    AOP  and Business Rules were added

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,9 @@ namespace Business.Constants
         public static string ProductNameInValid = "Ürün ismi geçersiz";
         internal static string MaintenanceTime="Sistem Bakımda";
         internal static string ProductsListed="Ürünler Listelendi";
+
+        public static string ProductCountOfCategoryError = "Bu kategori dolu";
+        public static string ProductNameAlreadyExists="Bu isimde zaten bir ürün var";
+        public static string CategoryLimitExceded="Kategori limiti aşıldığı için yeni ürün eklenemiyor";
     }
 }
